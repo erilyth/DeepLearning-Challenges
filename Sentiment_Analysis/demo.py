4ddeeb34e3e6a20eeff219b3caf39c9203137628
erilyth <vishal.batchu@students.iiit.ac.in>
2017-01-31 06:17:05 +0530

    Add semi trained model and update accuracy

--- Sentiment_Analysis/demo.py
+++ Sentiment_Analysis/demo.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 
 load_model = 0
+save_model = 0
 
 # Select only the two columns we require. Game title and its corresponding emotion
 dataframe = pd.read_csv('ign.csv').ix[:, 1:3]
@@ -39,12 +40,12 @@ trainX, testX, trainY, testY = train_test_split(totalX, totalY, test_size=0.1)
 net = tflearn.input_data([None, 15])
 # The 15 input word integers are then casted out into 256 dimensions each creating a word embedding.
 # We assume the dictionary has 10000 words maximum
-net = tflearn.embedding(net, input_dim=10000, output_dim=128)
+net = tflearn.embedding(net, input_dim=10000, output_dim=256)
 # Each input would have a size of 15x256 and each of these 256 sized vectors are fed into the LSTM layer one at a time.
 # All the intermediate outputs are collected and then passed on to the second LSTM layer.
-net = tflearn.gru(net, 128, dropout=0.9, return_seq=True)
+net = tflearn.gru(net, 256, dropout=0.9, return_seq=True)
 # Using the intermediate outputs, we pass them to another LSTM layer and collect the final output only this time
-net = tflearn.gru(net, 128, dropout=0.9)
+net = tflearn.gru(net, 256, dropout=0.9)
 # The output is then sent to a fully connected layer that would give us our final 11 classes
 net = tflearn.fully_connected(net, 11, activation='softmax')
 # We use the adam optimizer instead of standard SGD since it converges much faster
@@ -59,5 +60,6 @@ if load_model == 1:
 
 model.fit(trainX, trainY, validation_set=(testX, testY), show_metric=True, batch_size=32, n_epoch=20)
 
-model.save('gamemodel.tfl')
-print "Saved model!"
+if save_model == 1:
+	model.save('gamemodel.tfl')
+	print "Saved model!"
